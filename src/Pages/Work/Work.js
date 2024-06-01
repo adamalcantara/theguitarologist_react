c62a2e1e16885421082d36619191a3145510137a
adamalcantara <alcantaradevelopment@gmail.com>
2024-06-01 11:57:06 -0400

    Implementation of gallery works

--- src/Pages/Work/Work.js
+++ src/Pages/Work/Work.js
@@ -1,8 +1,18 @@
 import React from 'react'
 
+// import dependencies
+import PhotoAlbum from 'react-photo-album';
+import Lightbox from 'yet-another-react-lightbox';
+
+import photos from "./photos.ts";
+
 const Work = () => {
+  const [index, setIndex] = React.useState(-1);
+
   return (
-    <div>Work page here</div>
+    <div>
+      <PhotoAlbum photos={photos} layout='rows' targetRowHeight={150} onClick={({ index: current }) => setIndex(current)}/>
+    </div>
   )
 }
 
