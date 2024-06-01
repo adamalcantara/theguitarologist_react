ad264b8da67119694bc29595496e2870c1290419
adamalcantara <alcantaradevelopment@gmail.com>
2024-06-01 11:57:26 -0400

    Implementation of lightbox works

--- src/Pages/Work/Work.js
+++ src/Pages/Work/Work.js
@@ -1,19 +1,32 @@
-import React from 'react'
+import * as React from 'react'
+import "yet-another-react-lightbox/styles.css"
 
 // import dependencies
 import PhotoAlbum from 'react-photo-album';
 import Lightbox from 'yet-another-react-lightbox';
 
-import photos from "./photos.ts";
+import photos from "../../Data/photos.ts";
 
 const Work = () => {
   const [index, setIndex] = React.useState(-1);
 
   return (
     <div>
-      <PhotoAlbum photos={photos} layout='rows' targetRowHeight={150} onClick={({ index: current }) => setIndex(current)}/>
+      <PhotoAlbum 
+        layout='rows' 
+        photos={photos} 
+        targetRowHeight={200} 
+        onClick={({ index: current }) => setIndex(current)}
+       />
+
+      <Lightbox 
+        index={index} 
+        slides={photos} 
+        open={index >= 0} 
+        close={() => setIndex(-1)}
+      />
     </div>
-  )
+  );
 }
 
 export default Work
